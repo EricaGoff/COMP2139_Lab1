c0b6334d76ddb70a283b90125961feb639e82f37
Erica <ericarigby@outlook.com>
2025-10-11 15:19:12 -0400

    Lab 2 Completed – Projects Feature

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using COMP2139_Lab1.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace COMP2139_Lab1.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private static List<Project> Projects = new List<Project>();
+
+        public IActionResult Index()
+        {
+            return View(Projects);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                project.Id = Projects.Count + 1;
+                Projects.Add(project);
+                return RedirectToAction("Index");
+            }
+            return View(project);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var project = Projects.FirstOrDefault(p => p.Id == id);
+            if (project == null) return NotFound();
+            return View(project);
+        }
+    }
+}
---
+++ Models/Project.cs
@@ -0,0 +1,12 @@
+﻿namespace COMP2139_Lab1.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Owner { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
