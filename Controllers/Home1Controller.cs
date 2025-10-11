b73aa31957228afeaec353203591e18a6a8792cc
Erica <Ericarigby@outlook.com>
2025-10-11 15:01:33 -0400

    -

---
+++ Controllers/Home1Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP2139_Lab1.Controllers
+{
+    public class Home1Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
